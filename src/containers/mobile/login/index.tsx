aefa72bb97fa516b2e32e84e14dcf3ff4bccb625
GannaHup <gannahap02@gmail.com>
2024-12-27 18:19:46 +0700

    feat: [GAS-103] validation login when email or password incorrect

--- src/containers/mobile/login/index.tsx
+++ src/containers/mobile/login/index.tsx
@@ -1,9 +1,11 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Button, Flex, Input, Text } from "astarva-ui";
+import { AxiosError } from "axios";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
+import { Alert } from "@/components/base/Alert";
 import Layout from "@/components/mobile/Layout";
 import { KEY_ACCESS_TOKEN } from "@/constants/cookies";
 import { route } from "@/constants/route";
@@ -13,13 +15,17 @@ import { schemaLogin } from "@/modules/auth/schema/auth";
 import { cookies } from "@/utils/cookies";
 
 const LoginContainer: React.FC = () => {
+  const [errorMessage, setErrorMessage] = useState("");
   const router = useRouter();
-  const { isPending, mutate } = useLogin({
+  const { isPending, isError, mutate } = useLogin({
     onSuccess: ({ data }) => {
       if (data.token) {
         cookies.setCookie({ key: KEY_ACCESS_TOKEN, value: data.token });
         router.push(route.activityApp.home.path);
       }
+    },
+    onError: ({ response }) => {
+      setErrorMessage((response?.data as AxiosError).message);
     },
   });
   const {
@@ -34,6 +40,10 @@ const LoginContainer: React.FC = () => {
     mutate(form);
   };
 
+  const handleCloseAlert = () => {
+    setErrorMessage("");
+  };
+
   return (
     <Layout
       isShowBottomBar={false}
@@ -44,7 +54,16 @@ const LoginContainer: React.FC = () => {
           Login Here
         </Text>
       </Flex>
+
       <Flex flexDirection="column" padding="1.5rem" gap="1rem">
+        {isError && errorMessage && (
+          <Alert
+            message={errorMessage}
+            variant="error"
+            onClose={handleCloseAlert}
+          />
+        )}
+
         <Input
           label="Email"
           autoComplete="new-email"
