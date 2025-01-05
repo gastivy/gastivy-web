3ca8d70d800be25f3042a4e8f6c0e9ed66cc1bad
GannaHup <gannahap02@gmail.com>
2025-01-05 21:26:54 +0700

    fix: [GAS-45] expand description transaction

--- src/components/mobile/Transactions/CardTransaction.tsx
+++ src/components/mobile/Transactions/CardTransaction.tsx
@@ -1,4 +1,4 @@
-import { colorIndex, Flex, Icon, Text } from "astarva-ui";
+import { colorIndex, Flex, Icon, Text, useDisclosure } from "astarva-ui";
 import React from "react";
 
 import { TypesTransactions } from "@/modules/financeApp/category/models";
@@ -14,6 +14,8 @@ export const CardTransaction: React.FC<CardTransactionProps> = ({
   transaction,
   onClick,
 }) => {
+  const descriptionDisclosure = useDisclosure({ open: false });
+
   const getValueTransaction = (type: TypesTransactions, value?: number) => {
     if ([TypesTransactions.INCOME, TypesTransactions.PROFIT].includes(type)) {
       return {
@@ -45,55 +47,87 @@ export const CardTransaction: React.FC<CardTransactionProps> = ({
 
   return (
     <Flex
+      flexDirection="column"
       boxShadow="0 .0625rem .75rem .0625rem rgba(50, 132, 255, 0.1)"
-      padding=".625rem 1rem"
       borderRadius=".5rem"
-      justifyContent="space-between"
-      onClick={onClick}
     >
-      <Flex flexDirection="column" gap=".25rem">
-        <Text variant="small">{transaction.name}</Text>
-        <Text color="black300" variant="extra-small">
-          {transaction.category_name}
-        </Text>
-
-        {[TypesTransactions.INCOME, TypesTransactions.PROFIT].includes(
-          transaction.type
-        ) && (
-          <Text variant="extra-small" color="blue400" marginTop=".25rem">
-            {transaction.to_wallet_name}
+      <Flex
+        flex={1}
+        padding=".625rem 1rem"
+        justifyContent="space-between"
+        onClick={onClick}
+      >
+        <Flex flexDirection="column" gap=".25rem">
+          <Text variant="small">{transaction.name}</Text>
+          <Text color="black300" variant="extra-small">
+            {transaction.category_name}
           </Text>
-        )}
 
-        {[
-          TypesTransactions.EXPENSES,
-          TypesTransactions.FEE_TRANSFER,
-          TypesTransactions.LOSS,
-        ].includes(transaction.type) && (
-          <Text variant="extra-small" color="blue400" marginTop=".25rem">
-            {transaction.from_wallet_name}
-          </Text>
-        )}
-
-        {transaction.type === TypesTransactions.TRANSFER && (
-          <Flex alignItems="center" gap=".25rem" marginTop=".25rem">
-            <Text variant="extra-small" color="blue400">
-              {transaction.from_wallet_name}
-            </Text>
-            <Icon name="Arrow-Right-outline" size="1rem" />
-            <Text variant="extra-small" color="blue400">
+          {[TypesTransactions.INCOME, TypesTransactions.PROFIT].includes(
+            transaction.type
+          ) && (
+            <Text variant="extra-small" color="blue400" marginTop=".25rem">
               {transaction.to_wallet_name}
             </Text>
-          </Flex>
-        )}
+          )}
+
+          {[
+            TypesTransactions.EXPENSES,
+            TypesTransactions.FEE_TRANSFER,
+            TypesTransactions.LOSS,
+          ].includes(transaction.type) && (
+            <Text variant="extra-small" color="blue400" marginTop=".25rem">
+              {transaction.from_wallet_name}
+            </Text>
+          )}
+
+          {transaction.type === TypesTransactions.TRANSFER && (
+            <Flex alignItems="center" gap=".25rem" marginTop=".25rem">
+              <Text variant="extra-small" color="blue400">
+                {transaction.from_wallet_name}
+              </Text>
+              <Icon name="Arrow-Right-outline" size="1rem" />
+              <Text variant="extra-small" color="blue400">
+                {transaction.to_wallet_name}
+              </Text>
+            </Flex>
+          )}
+        </Flex>
+        <Text
+          variant="small"
+          color={getValueTransaction(transaction.type)?.color}
+          weight="medium"
+        >
+          {getValueTransaction(transaction.type, transaction.money)?.money}
+        </Text>
       </Flex>
-      <Text
-        variant="small"
-        color={getValueTransaction(transaction.type)?.color}
-        weight="medium"
-      >
-        {getValueTransaction(transaction.type, transaction.money)?.money}
-      </Text>
+
+      {transaction.description && (
+        <Flex flexDirection="column">
+          <Flex
+            justifyContent="center"
+            paddingY=".25rem"
+            borderTop=".0625rem solid"
+            borderColor="black100"
+            borderRadius=".5rem"
+            onClick={descriptionDisclosure.onToggle}
+          >
+            <Icon
+              name={
+                descriptionDisclosure.isOpen ? "Up-outline" : "Down-outline"
+              }
+            />
+          </Flex>
+
+          {descriptionDisclosure.isOpen && (
+            <Flex padding=".625rem 1rem">
+              <Text variant="small" color="black500">
+                {transaction.description}
+              </Text>
+            </Flex>
+          )}
+        </Flex>
+      )}
     </Flex>
   );
 };
